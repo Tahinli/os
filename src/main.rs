326eaf2c44cd3eb02cd17ca1d7899d91d114a36f
Ahmet Kaan Gümüş <tahinli@tahinli.com>
2025-09-19 00:44:33 +0300

    feat: :sparkles: configure test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 use core::panic::PanicInfo;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn example_test() {
+    assert_eq!(1, 1);
+}
 
 #[panic_handler]
 pub fn panic_handler(info: &PanicInfo) -> ! {
@@ -13,6 +29,9 @@ pub fn panic_handler(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello from TahinliOS!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {
         panic!("Panic or not, it's the whole thing.");
